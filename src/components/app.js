7f045b90b675b2678f943d940b596dcde70132ac
Michael Glenn <dmichaelglenn@gmail.com>
2017-02-06 12:51:36 -0800

    building out booklist container

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import BookList from '../containers/book-list';
 export default class App extends Component {
   render() {
     return (
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+    return [
+        {title: "The Dark Lord"},
+        {title: "JS the Weird Parts"},
+        {title: "Eleoquent Ruby"}
+    ]
+}
